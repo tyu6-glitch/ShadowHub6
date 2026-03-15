6075da11374cba2a18bb5e9cf10355656f401023
YourName <your-email@example.com>
2026-03-15 23:33:47 +0300

    تحديث الأكواد

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\PCD-hubeix\Desktop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\PCD-hubeix\Desktop\andr\aaaassss"
+export "FLUTTER_APPLICATION_PATH=C:\Users\PCD-hubeix\Desktop\nexusHub_App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
